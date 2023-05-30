fe9d730ab88764d8beb7dca570411d4b09e6ffa6
Diego Hessel <diego.verdi@sptech.school>
2023-05-29 21:24:56 -0300

    plotar primeiro grafico

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -1,6 +1,5 @@
 create database individuall;
 use individuall;
-select * from cadastro;
 
 create table jogador(
 idjogador int primary key auto_increment,
@@ -21,12 +20,14 @@ senha varchar(100),
 fkjogador int, constraint fkjogo foreign key (fkjogador) references jogador (idjogador)
 ); 
 
+
+
  insert into jogador values
  (null,'DUDU',31,11,86,427,'ATACANTE'),
  (null,'WEVERTON',35, 10,0,295,'GOLEIRO'),
  (NULL,'RAPHAEL VEIGA',27 ,9 ,75,231,'MEIA'),
  (NULL,'GUSTAVO SCARPA',29 , 8,44,230,'MEIA'),
-(NULL,'GUSTAVO GOMEZ',31,10,31,243,'zageiro'),
+(NULL,'GUSTAVO GOMEZ',31,10,31,243,'ZAGUEIRO'),
 (NULL,'FERNANDO PRASS',44,4,1,274,'GOLEIRO'),
 (NULL,'MARCOS',49,12,0,533,'GOLEIRO'),
 (NULL,'ZÉ ROBERTO',48,2,10,133,'MEIA'),
@@ -34,3 +35,22 @@ fkjogador int, constraint fkjogo foreign key (fkjogador) references jogador (idj
 (NULL,'MARCOS ASSUNÇÃO',46,1,31,145,'VOLANTE'),
 (NULL,'EDMUNDO',51,5,99,223,'ATACANTE'),
 (NULL,'ALEX',45,4,78,243,'MEIA');
+
+select *from cadastro join jogador on idjogador = fkjogador where idcadastro =5;
+select jogador.nome, count(fkjogador) from jogador join cadastro on idjogador= fkjogador group by(jogador.nome);
+select * from jogador;
+select * from cadastro;
+select* FROM jogador join cadastro on idjogador =  fkjogador ;
+select count(fkjogador) from cadastro where fkjogador = 1;
+select count(fkjogador) from cadastro where fkjogador = 2;
+select count(fkjogador) from cadastro where fkjogador = 3;
+select count(fkjogador) from cadastro where fkjogador = 4;
+select count(fkjogador) from cadastro where fkjogador = 5;
+select count(fkjogador) from cadastro where fkjogador = 6;
+select count(fkjogador) from cadastro where fkjogador = 7;
+select count(fkjogador) from cadastro where fkjogador = 8;
+select count(fkjogador) from cadastro where fkjogador = 9;
+select count(fkjogador) from cadastro where fkjogador = 10;
+select count(fkjogador) from cadastro where fkjogador =11;
+select count(fkjogador) from cadastro where fkjogador =12;
+
--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,23 +5,13 @@ function buscarUltimasMedidas(idAquario, limite_linhas) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
+        instrucaoSql = `select jogador.nome, count(fkjogador) 
+        as votos from jogador join cadastro on idjogador = fkjogador
+         group by jogador.nome;`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `select jogador.nome, count(fkjogador) 
+        as votos from jogador join cadastro on idjogador = fkjogador
+         group by jogador.nome;`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -36,22 +26,14 @@ function buscarMedidasEmTempoReal(idAquario) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
+        instrucaoSql = `select jogador.nome, count(fkjogador) 
+        as votos from jogador join cadastro on idjogador = fkjogador
+         group by jogador.nome;`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
+        instrucaoSql = `select jogador.nome, count(fkjogador) 
+        as votos from jogador join cadastro on idjogador = fkjogador
+         group by jogador.nome;`;;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
